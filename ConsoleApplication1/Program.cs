332ff90f3fb93fcf68a3bcfe5cd213e75aecb9c0
DZero <DZero@DZERO-PC>
2017-02-14 23:38:13 +0800

    添加项目文件。

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> x = 1;
+            int? y = null;
+            y++;
+            Console.WriteLine("{0}{1}{2}{3}", x.HasValue, x.GetValueOrDefault(), y.HasValue, y.GetValueOrDefault());
+            if (x<y)
+            {
+                Console.WriteLine("X<y");
+            }
+            else
+            {
+                Console.WriteLine("x>y");
+            }
+            Console.ReadLine();
+        }
+    }
+}
