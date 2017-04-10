c614a0b5309b919fe80cfa094381263bb114277b
ZeroDai <624175217@qq.com>
2017-04-10 10:36:16 +0800

    界面制作

--- Wpf_CrazyE_Demo/ViewModels/MainWindowViewModel.cs
+++ Wpf_CrazyE_Demo/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿
 using Prism.Commands;
 using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -26,67 +27,67 @@ namespace Wpf_CrazyE_Demo.ViewModels
             }
         }
 
-        private Restaurant resaurant;
+        private Restaurant restaurant;
 
-        public Restaurant Resaurant
+        public Restaurant Restaurant
         {
-            get { return resaurant; }
+            get { return restaurant; }
             set
             {
-                resaurant = value;
-                this.RaisePropertyChanged("Resaurant");
+                restaurant = value;
+                this.RaisePropertyChanged("Restaurant");
             }
         }
 
-        private List<DishMenuItemViewModel> disMenu;
+        private List<DishMenuItemViewModel> dishMenu;
 
-        public List<DishMenuItemViewModel> DisMenu
+        public List<DishMenuItemViewModel> DishMenu
         {
-            get { return disMenu; }
+            get { return dishMenu; }
             set
             {
-                disMenu = value;
-                this.RaisePropertyChanged("DisMenu");
+                dishMenu = value;
+                this.RaisePropertyChanged("DishMenu");
             }
         }
         public MainWindowViewModel()
         {
             this.LoadResaurant();
             this.LoadDishMenu();
-            this.PlaceOrderCommand = new DelegateCommand(new System.Action(this.PlaceOrderCommandExecute));
-            this.SelectMenuItemCommand = new DelegateCommand(new System.Action(this.SelectMenuItemExecute));
+            this.PlaceOrderCommand = new DelegateCommand(new Action(this.PlaceOrderCommandExecute));
+            this.SelectMenuItemCommand = new DelegateCommand(new Action(this.SelectMenuItemExecute));
         }
 
         private void LoadResaurant()
         {
-            this.Resaurant = new Restaurant();
-            this.Resaurant.Name = "Crazy猴子";
-            this.Resaurant.Address = "连云港花果山水帘洞";
-            this.Resaurant.PhoneNumber = "87878787";
+            this.Restaurant = new Restaurant();
+            this.Restaurant.Name = "Crazy猴子";
+            this.Restaurant.Address = "连云港花果山水帘洞";
+            this.Restaurant.PhoneNumber = "87878787";
         }
 
         private void LoadDishMenu()
         {
             XmlDataService ds = new XmlDataService();
             var dishes = ds.GetAllDishes();
-            this.DisMenu = new List<DishMenuItemViewModel>();
+            this.DishMenu = new List<DishMenuItemViewModel>();
             foreach (var dish in dishes)
             {
                 DishMenuItemViewModel item = new DishMenuItemViewModel();
                 item.Dish = dish;
-                this.DisMenu.Add(item);
+                this.DishMenu.Add(item);
             }
         }
         private void PlaceOrderCommandExecute()
         {
-            var selectDishs = this.DisMenu.Where(i => i.IsSelected == true).Select(i => i.Dish.Name).ToList();
+            var selectDishs = this.DishMenu.Where(i => i.IsSelected == true).Select(i => i.Dish.Name).ToList();
             MockOrderService orderservice = new MockOrderService();
             orderservice.PlaceOrder(selectDishs);
             MessageBox.Show("订餐成功！");
         }
         private void SelectMenuItemExecute()
         {
-            this.Count = this.DisMenu.Count(i => i.IsSelected == true);
+            this.Count = this.DishMenu.Count(i => i.IsSelected == true);
         }
 
 
