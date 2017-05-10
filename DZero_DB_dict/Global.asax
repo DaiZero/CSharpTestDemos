b0be5db15d942e6f604e07f54453542959c16c8c
ZeroDai <624175217@qq.com>
2017-05-10 16:13:41 +0800

    add

---
+++ DZero_DB_dict/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DZero_DB_dict.MvcApplication" Language="C#" %>
---
+++ DZero_DB_dict/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DZero_DB_dict.Startup))]
+namespace DZero_DB_dict
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
