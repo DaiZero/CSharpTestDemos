cb7e44261a9202f7c90fb6805555e3980998e948
ZeroDai <624175217@qq.com>
2017-05-16 17:38:49 +0800

    add

--- WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_Client/Program.cs
+++ WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_Client/Program.cs
@@ -10,14 +10,52 @@ namespace DZero_Wcf_CalculatorService_Client
     {
         static void Main(string[] args)
         {
+            string strNum = ChooseNumber();
+            Write(strNum);
+            Console.ReadKey();
+        }
+
+        public static string ChooseNumber()
+        {
+            Console.WriteLine("---------Choose the Fuction----------");
+            Console.WriteLine("---------输入数字 1 进入 加法------- ");
+            Console.WriteLine("---------输入数字 2 进入 减法------- ");
+            Console.WriteLine("---------输入数字 3 进入 乘法------- ");
+            Console.WriteLine("---------输入数字 4 进入 除法----- ");
+            Console.WriteLine("----------------------------------- ");
+            Console.Write("---------输入数字:");
+            return Console.ReadLine().ToString();
+        }
+
+        public static void Write(string strChooseNo)
+        {
             using (CalculatorServiceClient proxy = new CalculatorServiceClient())
             {
-                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
-                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
-                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
-                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
-                Console.ReadLine();
+                switch (strChooseNo)
+                {
+                    case "1":
+                        Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                        Write(ChooseNumber());
+                        break;
+                    case "2":
+                        Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                        Write(ChooseNumber());
+                        break;
+                    case "3":
+                        Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                        Write(ChooseNumber());
+                        break;
+                    case "4":
+                        Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                        Write(ChooseNumber());
+                        break;
+                    default:
+                        Console.WriteLine("输入错误，请重新输入数字！");
+                        Write(ChooseNumber());
+                        break;
+                }
             }
         }
+
     }
 }
--- WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_Services/CalculatorService.cs
+++ WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_Services/CalculatorService.cs
@@ -8,33 +8,34 @@ namespace DZero_Wcf_CalculatorService_Services
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
     public class CalculatorService : ICalculator
     {
-        
+        string machineName = Environment.MachineName;
         public double Add(double x, double y)
         {
             MethodBase m = new StackTrace().GetFrame(0).GetMethod();
-            Console.WriteLine($"{m}方法被使用！");
+            Console.WriteLine($"{machineName} 正在使用{m}方法被使用！");
             return x + y;
         }
 
         public double Divide(double x, double y)
         {
             MethodBase m = new StackTrace().GetFrame(0).GetMethod();
-            Console.WriteLine($"{m}方法被使用！");
+            Console.WriteLine($"{machineName} 正在使用{m}方法被使用！");
             return x - y;
         }
 
         public double Multiply(double x, double y)
         {
             MethodBase m = new StackTrace().GetFrame(0).GetMethod();
-            Console.WriteLine($"{m}方法被使用！");
+            Console.WriteLine($"{machineName} 正在使用{m}方法被使用！");
             return x * y;
         }
 
         public double Subtract(double x, double y)
         {
             MethodBase m = new StackTrace().GetFrame(0).GetMethod();
-            Console.WriteLine($"{m}方法被使用！");
+            Console.WriteLine($"{machineName} 正在使用{m}方法被使用！");
             return x / y;
         }
+
     }
 }
