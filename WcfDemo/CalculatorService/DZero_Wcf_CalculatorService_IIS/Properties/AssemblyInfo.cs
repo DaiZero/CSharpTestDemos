8b1e167c068409e8885c50661e7222a91b966096
ZeroDai <624175217@qq.com>
2017-05-17 17:48:13 +0800

    wcf to iis

--- WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_IIS/Properties/AssemblyInfo.cs
+++ WcfDemo/CalculatorService/DZero_Wcf_CalculatorService_IIS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DZero_Wcf_CalculatorService_Hosting_iis")]
+[assembly: AssemblyTitle("DZero_Wcf_CalculatorService_IIS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DZero_Wcf_CalculatorService_Hosting_iis")]
+[assembly: AssemblyProduct("DZero_Wcf_CalculatorService_IIS")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8de18ec2-4e73-437d-9d9e-81bc1527f8fb")]
+[assembly: Guid("07afecd2-9da8-4a07-842b-fab6974ff0d8")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
